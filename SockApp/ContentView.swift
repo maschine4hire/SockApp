853fb7b1d5011b1b672814b7fbf607c065184f58
maschine4hire <mariamart31@gmail.com>
2025-09-08 13:00:03 +0200

    Initial Commit

---
+++ SockApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SockApp
+//
+//  Created by masch.ine on 08.09.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SockApp/SockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SockAppApp.swift
+//  SockApp
+//
+//  Created by masch.ine on 08.09.25.
+//
+
+import SwiftUI
+
+@main
+struct SockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
