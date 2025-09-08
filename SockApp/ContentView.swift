06151ce2016c5923edb092122f7a73623a651628
maschine4hire <mariamart31@gmail.com>
2025-09-08 13:14:52 +0200

    here is a start

--- SockApp/ContentView.swift
+++ SockApp/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        ZStack{
+            RadialGradient(gradient: Gradient(colors: [Color.yellow, Color.red]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, startRadius: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/, endRadius: /*@START_MENU_TOKEN@*/500/*@END_MENU_TOKEN@*/)
+                .cornerRadius(15)
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+         
+                
+                Text("here is an app, that helps you with your sock knitting needs")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                Text("or at least it tries")
+        }.padding()
+        }.ignoresSafeArea()
+       
     }
 }
 
